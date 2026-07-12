73a03939a9bdb88380926f2eb8452354b4c558e8
Evan Whitfield <Evan>
2026-07-12 18:24:28 -0400

    Changed Mind on Graphs

--- app.R
+++ app.R
@@ -6,29 +6,44 @@ library(ggplot2)
 library(shiny)
 library(DT)
 library(bslib)
-library(shinydashboard)
 
-nfl <- read_csv("data/NFL_play_data.csv")
-
-
-ui <- dashboardPage(
-  dashboardHeader(title="NFL Play Data (2009-2016)"),
-  
-  dashboardSidebar(    
-    sidebarMenu(
-      menuItem("Play Type vs. Gain", tabName = "scatplot", icon = icon("archive")),
-      menuItem("Play Type Breakdown", tabName = "breakdown", icon = icon("laptop"))
-    )
-  ),
-  
-  dashboardBody(
-    tabItems(
-      tabItem(tabName = "scatplot",
-              titlePanel("Play Type vs. Gain"),
-      ),
-      tabItem(tabName = "breakdown",
-              titlePanel("Play Type Breakdown"),
+nfl_data <- read_csv("data/NFL_play_data.csv") |>
+  rename(yards_gained = Yards.Gained) |>
+  #Need to change the name of a couple of teams to make sure that there are only 32 teams listed.
+  mutate(
+    across(
+      c(posteam, DefensiveTeam, HomeTeam, AwayTeam),
+      ~ recode(
+        .x,
+        "STL" = "LA",
+        "JAC" = "JAX"
       )
+    )
+  )
+
+nfl_divisions <- list(
+  
+  AFC_North = c("BAL", "CIN", "CLE", "PIT"),
+  AFC_South = c("HOU", "IND", "JAX", "TEN"),
+  AFC_East  = c("BUF", "MIA", "NE" , "NYJ"),
+  AFC_West  = c("DEN", "KC" , "LAC", "OAK"),
+  NFC_North = c("CHI", "DET", "GB" , "MIN"),
+  NFC_South = c("ATL", "CAR", "NO" , "TB" ),
+  NFC_East  = c("DAL", "NYG", "PHI", "WAS"),
+  NFC_West  = c("ARI", "LA" , "SF" , "SEA")
+  )
+
+ui <- fluidPage(
+  pageWithSidebar(
+    headerPanel("NFL Play Data (2009-2016)"),
+    sidebarPanel(
+      selectInput("team",
+                "Select Team",
+                sort(unique(nfl_data$HomeTeam))
+      )
+    ),
+    card(
+      plotOutput(outputId = "histogram")
     )
   )
 )
@@ -36,6 +51,25 @@ ui <- dashboardPage(
 
 server <- function(input, output, session){
   
+  output$histogram <- renderPlot({
+    
+    req(input$team)
+    
+    nfl_data |>
+      filter(posteam == input$team,
+             !is.na(yards_gained)) |>
+      ggplot(aes(x = yards_gained)) +
+      geom_histogram(
+        binwidth = 5,
+        fill = "steelblue",
+        color = "black"
+      ) +
+      labs(
+        title = paste("Distribution of Yards Gained by", input$team),
+        x = "Yards Gained",
+        y = "Count"
+      )
+  })
   
 }
 
